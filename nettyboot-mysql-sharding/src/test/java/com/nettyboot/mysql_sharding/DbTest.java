cbe0d5182b6874a2fbffe5d90075d70a56a9efc6
limengrui <mengrui.li@jutaol.com>
2021-06-04 13:56:58 +0800

    update nettyboot-mysql-sharding

--- nettyboot-mysql-sharding/src/test/java/com/nettyboot/mysql_sharding/DbTest.java
+++ nettyboot-mysql-sharding/src/test/java/com/nettyboot/mysql_sharding/DbTest.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.nettyboot.mysql_sharding.config.ShardingType;
 import com.nettyboot.util.FileUtil;
+import org.antlr.v4.runtime.atn.SemanticContext.AND;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -31,13 +32,11 @@ import java.util.Properties;
 public class DbTest {
 
     private static final Logger logger = LoggerFactory.getLogger(DbTest.class);
-    private static final String PROPERTIES_FILEPATH = "/application.properties";
     private static DataSource dataSource = null;
     private static Connection connection = null;
 
     @Before
     public void init() throws IOException, SQLException {
-        Properties properties = FileUtil.getProperties(PROPERTIES_FILEPATH);
         dataSource = YamlDataSourceFactory.newInstance(ShardingType.SHARDING_DATABASES, "/demo/test-sharding-databases-range.yaml");
 
         connection = dataSource.getConnection();
@@ -53,13 +52,170 @@ public class DbTest {
 
 
     @Test
-    public void test(){
+    public void testSql(){
 //        String sql = "SELECT * FROM t_order o RIGHT JOIN (SELECT MAX(`order_id`) AS `max_order_id` FROM t_order where ymd > '2019-12' and ymd <= '2021-02' ) AS tmp on o.`order_id`=tmp.max_order_id  where ymd > '2019-12' and ymd <= '2021-02'";
-        String sql = "SELECT MAX(`order_id`) AS `max_order_id` FROM t_order where ymd > '2019-12' and ymd <= '2021-02'";
+        String sql = "SELECT MAX(`id`) AS `max_id` FROM user_info where ymd > '2019-12' and ymd <= '2021-02'";
 
         JSONArray objects = selectBySQL(sql);
         System.out.println(objects);
+    }
 
+    @Test
+    public void testSql2(){
+//        String sql = "SELECT * FROM t_order o RIGHT JOIN (SELECT MAX(`order_id`) AS `max_order_id` FROM t_order where ymd > '2019-12' and ymd <= '2021-02' ) AS tmp on o.`order_id`=tmp.max_order_id  where ymd > '2019-12' and ymd <= '2021-02'";
+        String sql = "SELECT MAX(u.`id`) AS `max_id` FROM user_info AS u Left join user_info_more as um on u.id=um.id where u.ymd > '2020-09' and u.ymd <= '2021-03'";
+
+        JSONArray objects = selectBySQL(sql);
+        System.out.println(objects);
+    }
+
+    @Test
+    public void testSql3(){
+//        String sql = "SELECT * FROM t_order o RIGHT JOIN (SELECT MAX(`order_id`) AS `max_order_id` FROM t_order where ymd > '2019-12' and ymd <= '2021-02' ) AS tmp on o.`order_id`=tmp.max_order_id  where ymd > '2019-12' and ymd <= '2021-02'";
+        String sql = "SELECT * FROM user_info_current limit 10";
+        for (int i = 0; i < 10; i++) {
+            JSONArray objects = selectBySQL(sql);
+            System.out.println(objects);
+        }
+    }
+
+    @Test
+    public void testSql4(){
+
+        String sql = "SELECT `v`.`top_industryid`,`vb`.`industryid`,v.top_industry_name,vb.industry_name,v.video_tags,\n" +
+                "      v.video_view_count,v.video_like_count,v.video_forward_count\n" +
+                "      FROM `video_info_current` AS `v` INNER JOIN `video_industry2_rel` AS `vb` ON `v`.`video_aid` = `vb`.`videoid`\n" +
+                "      WHERE v.`publish_ymd` > '2021-01-01'  AND v.`publish_ymd` <= '2021-05-01'  AND `vb`.`industryid` = 1";
+
+        JSONArray objects = selectBySQL(sql);
+        System.out.println(objects);
+    }
+
+    @Test
+    public void testSql5(){
+
+        String sql = "SELECT `v`.`top_industryid`,`vb`.`industryid`,v.top_industry_name,vb.industry_name,v.video_tags,\n" +
+                "      v.video_view_count,v.video_like_count,v.video_forward_count\n" +
+                "      FROM `video_info_current` AS `v` INNER JOIN `video_industry2_rel` AS `vb` ON `v`.`video_aid` = `vb`.`videoid`\n" +
+                "      WHERE v.`publish_ymd` > '2021-01-01'  AND v.`publish_ymd` <= '2021-05-01'  AND `vb`.`industryid` = 1";
+
+        JSONArray objects = selectBySQL(sql);
+        System.out.println(objects);
+    }
+
+    @Test
+    public void testSql6(){
+
+        String sql = "SELECT `v`.`top_industryid`,`vb`.`industryid`,v.top_industry_name,vb.industry_name,v.video_tags,\n" +
+                "      v.video_view_count,v.video_like_count,v.video_forward_count\n" +
+                "      FROM `video_info` AS `v` INNER JOIN `video_industry2_rel` AS `vb` ON `v`.`video_aid` = `vb`.`videoid`\n" +
+                "      WHERE v.`ymd` > '2020-12-01'  AND v.`ymd` <= '2021-05-01'  AND `vb`.`industryid` = 1";
+
+        JSONArray objects = selectBySQL(sql);
+        System.out.println(objects);
+    }
+
+    @Test
+    public void testSql7(){
+
+        String sql = "SELECT `v`.`top_industryid`,`vb`.`brandid`,v.top_industry_name,vb.brand_name,v.video_tags,\n" +
+                "      v.video_view_count,v.video_like_count,v.video_forward_count\n" +
+                "      FROM `video_info` AS `v` INNER JOIN `video_brand_rel` AS `vb` ON `v`.`video_aid` = `vb`.`videoid`\n" +
+                "      WHERE v.`ymd` > '2020-12-01'  AND v.`ymd` <= '2021-05-01'  AND `vb`.`brandid` = 1";
+
+        JSONArray objects = selectBySQL(sql);
+        System.out.println(objects);
+    }
+
+    @Test
+    public void testSql8(){
+
+        String sql = "SELECT\n" +
+                "\t`u`.`ymd`,\n" +
+                "            `view_count`,\n" +
+                "            `like_count`,\n" +
+                "            `collect_count`,\n" +
+                "            `um`.`comment_count`,\n" +
+                "            `video_count`,\n" +
+                "            `fans_count`,\n" +
+                "            `view_count`,\n" +
+                "            `danmaku_count`,\n" +
+                "            `forward_count`,\n" +
+                "            `coin_count`,\n" +
+                "    row_number ( ) over ( PARTITION BY `u`.`ymd` ORDER BY `u`.`id` DESC ) AS `rn`\n" +
+                "    FROM\n" +
+                "\t`user_info` AS `u`\n" +
+                "    LEFT JOIN `user_info_more` AS `um` ON `u`.`userid` = `um`.`userid`\n" +
+                "    WHERE\n" +
+                "`u`.`ymd` IN ( '2021-03-19', '2021-04-28' )\n" +
+                "    AND `u`.userid = '10040906'";
+
+        JSONArray objects = selectBySQL(sql);
+        System.out.println(objects);
+    }
+
+
+    @Test
+    public void testSql9(){
+
+        String sql = "SELECT " +
+                "`u`.`ymd`,`view_count`,`like_count`,`collect_count`,`um`.`comment_count`,`video_count`,`fans_count`," +
+                "`view_count`,`danmaku_count`,`forward_count`,`coin_count`  " +
+                "FROM `user_info` AS `u`  " +
+                "INNER JOIN (  " +
+                "SELECT MAX(`id`) AS `max_id`  " +
+                "FROM `user_info` AS `u`  " +
+                "WHERE `userid` = '352049471'  " +
+                "AND `ymd` IN ('2021-02-17','2021-04-28') GROUP BY `ymd` " +
+                ") AS `tmp` ON `u`.`id` = `tmp`.`max_id`  " +
+                "LEFT JOIN `user_info_more_202104` AS `um` on `u`.`userid`=`um`.`userid`  " +
+                "WHERE `u`.`ymd` IN ('2021-02-17','2021-04-28')";
+
+        System.out.println(sql);
+        JSONArray objects = selectBySQL(sql);
+        System.out.println(objects);
+    }
+
+
+    @Test
+    public void testSql10(){
+
+        String sql = "SELECT " +
+                "`u`.`ymd`,`view_count`,`like_count`,`collect_count`,`um`.`comment_count`,`video_count`,`fans_count`," +
+                "`view_count`,`danmaku_count`,`forward_count`,`coin_count`,row_number ( ) over ( PARTITION BY `u`.`ymd` ORDER BY `u`.`id` DESC ) AS `rn`  " +
+                "FROM `user_info` AS `u`  " +
+//                "LEFT JOIN (  " +
+//                "SELECT `id`  " +
+//                "FROM `user_info` AS `u`  " +
+//                "WHERE `userid` = '352049471'  " +
+//                "AND `ymd` IN ('2021-02-17','2021-04-28') " +
+//                ") AS `tmp` ON `u`.`id` = `tmp`.`max_id`  " +
+                "LEFT JOIN `user_info_more` AS `um` on `u`.`userid`=`um`.`userid`  " +
+                "WHERE `u`.`ymd` IN ('2021-02-17','2021-03-19')";
+
+        JSONArray objects = selectBySQL(sql);
+        System.out.println(objects);
+    }
+
+    @Test
+    public void testSql11(){
+
+        String sql = "SELECT " +
+                "`u`.`ymd`,`view_count`,`like_count`,`collect_count`,`um`.`comment_count`,`video_count`,`fans_count`," +
+                "`view_count`,`danmaku_count`,`forward_count`,`coin_count`  " +
+                "FROM `user_info` AS `u`  " +
+//                "INNER JOIN (  " +
+//                "SELECT MAX(`id`) AS `max_id`  " +
+//                "FROM `user_info` AS `u`  " +
+//                "WHERE `userid` = '352049471'  " +
+//                "AND `ymd` IN ('2021-02-17','2021-04-28') GROUP BY `ymd` " +
+//                ") AS `tmp` ON `u`.`id` = `tmp`.`max_id`  " +
+                "LEFT JOIN `user_info_more_202104` AS `um` on `u`.`userid`=`um`.`userid`  " +
+                "WHERE `u`.`ymd` IN ('2021-02-17','2021-04-28')" +
+                " AND `u`.`userid` = '352049471' ";
+
+        JSONArray objects = selectBySQL(sql);
+        System.out.println(objects);
     }
 
     public static JSONArray selectBySQL(String sql, Object... data) {
